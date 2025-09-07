1c0a95f39158528fed83ed457ada07b7b589a5da
natsuko-ak <natsuko_akuta3-std@yahoo.co.jp>
2025-09-07 15:43:19 +0900

    変数埋め込みの練習

--- app.py
+++ app.py
@@ -14,5 +14,17 @@ def index_test():
 def index_flask():
     return render_template("index.html")
 
+# 擬似的にDB代わりのデータ
+users = {
+    1: "Natsu",
+    2: "Taro",
+    3: "Hana"
+}
+
+@app.route('/user/<int:user_id>')
+def user_page(user_id):
+    name = users.get(user_id, "ゲスト") # IDがなければ「ゲスト」
+    return render_template("user.html", name=name)
+
 if __name__=="__main__":
     app.run(debug =True, port=5001)
